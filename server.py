68d36e1cdf7c3521c00bfefb19a9dcb8ac4dcd1c
MJR <mohammad.rafiei69@gmail.com>
2020-07-11 21:36:57 +0430

    use VideoTransformTrack to transform video frames with open cv

--- server.py
+++ server.py
@@ -11,6 +11,8 @@ from sanic import Sanic
 from sanic.response import json as json_response, html
 from sanic.websocket import WebSocketProtocol
 from uuid import uuid4
+import cv2
+from av import VideoFrame
 
 
 context = ssl.create_default_context(purpose=ssl.Purpose.CLIENT_AUTH)
@@ -22,6 +24,76 @@ app.static('/static', './static')
 logger = logging.getLogger("pc")
 
 pcs = set()
+
+
+class VideoTransformTrack(MediaStreamTrack):
+    """
+    A video stream track that transforms frames from an another track.
+    """
+
+    kind = "video"
+
+    def __init__(self, track, transform):
+        super().__init__()  # don't forget this!
+        self.track = track
+        self.transform = transform
+
+    async def recv(self):
+        frame = await self.track.recv()
+
+        if self.transform == "cartoon":
+            img = frame.to_ndarray(format="bgr24")
+
+            # prepare color
+            img_color = cv2.pyrDown(cv2.pyrDown(img))
+            for _ in range(6):
+                img_color = cv2.bilateralFilter(img_color, 9, 9, 7)
+            img_color = cv2.pyrUp(cv2.pyrUp(img_color))
+
+            # prepare edges
+            img_edges = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+            img_edges = cv2.adaptiveThreshold(
+                cv2.medianBlur(img_edges, 7),
+                255,
+                cv2.ADAPTIVE_THRESH_MEAN_C,
+                cv2.THRESH_BINARY,
+                9,
+                2,
+            )
+            img_edges = cv2.cvtColor(img_edges, cv2.COLOR_GRAY2RGB)
+
+            # combine color and edges
+            img = cv2.bitwise_and(img_color, img_edges)
+
+            # rebuild a VideoFrame, preserving timing information
+            new_frame = VideoFrame.from_ndarray(img, format="bgr24")
+            new_frame.pts = frame.pts
+            new_frame.time_base = frame.time_base
+            return new_frame
+        elif self.transform == "edges":
+            # perform edge detection
+            img = frame.to_ndarray(format="bgr24")
+            img = cv2.cvtColor(cv2.Canny(img, 100, 200), cv2.COLOR_GRAY2BGR)
+
+            # rebuild a VideoFrame, preserving timing information
+            new_frame = VideoFrame.from_ndarray(img, format="bgr24")
+            new_frame.pts = frame.pts
+            new_frame.time_base = frame.time_base
+            return new_frame
+        elif self.transform == "rotate":
+            # rotate image
+            img = frame.to_ndarray(format="bgr24")
+            rows, cols, _ = img.shape
+            M = cv2.getRotationMatrix2D((cols / 2, rows / 2), frame.time * 45, 1)
+            img = cv2.warpAffine(img, M, (cols, rows))
+
+            # rebuild a VideoFrame, preserving timing information
+            new_frame = VideoFrame.from_ndarray(img, format="bgr24")
+            new_frame.pts = frame.pts
+            new_frame.time_base = frame.time_base
+            return new_frame
+        else:
+            return frame
 
 
 @app.listener('before_server_start')
@@ -228,10 +300,10 @@ async def offer(request):
                 await asyncio.sleep(0.1)
             # pc.addTrack(track)
         elif track.kind == "video":
-            # local_video = VideoTransformTrack(
-            #     track, transform=params["video_transform"]
-            # )
-            app.chats[stream_key]['video_track'] = track
+            transformed_track = VideoTransformTrack(
+                track, transform=params["video_transform"]
+            )
+            app.chats[stream_key]['video_track'] = transformed_track
             while True:
                 if app.chats[peer]['video_track']:
                     pc.addTrack(app.chats[peer]['video_track'])
