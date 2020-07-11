f52837b78c397ab2ba96a2943d5cd5422c1a3968
MJR <mohammad.rafiei69@gmail.com>
2020-07-11 08:26:09 +0430

    use aiortc for sending and exchangingmedia

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 import asyncio
+import logging
 from asyncpg import create_pool
 from uuid import uuid4
 from sanic import Sanic
-from sanic.response import json, html
+from sanic.response import json as json_response, html
+from aiortc import MediaStreamTrack, RTCPeerConnection, RTCSessionDescription
+from aiortc.contrib.media import MediaBlackhole, MediaPlayer, MediaRecorder
+from queue import Queue
+import ssl
+import time
+
+context = ssl.create_default_context(purpose=ssl.Purpose.CLIENT_AUTH)
+context.load_cert_chain("ssl_keys/selfsigned.crt", keyfile="ssl_keys/selfsigned.key")
+
 
 app = Sanic("live_stream")
 app.static('/static', './static')
+logger = logging.getLogger("pc")
+
+pcs = set()
 
 
 @app.listener('before_server_start')
@@ -24,6 +37,32 @@ async def register_db(app, loop):
         loop=loop
     )
 
+    app.chats = {
+        'a495fba2': {'audio_track': None, 'video_track': None, 'peer': '34551d92'},
+        '34551d92': {'audio_track': None, 'video_track': None, 'peer': 'a495fba2'},
+    }
+
+    app.add_track_queue = Queue()
+
+
+async def notify_server_started_after_five_seconds(app):
+    while True:
+        if not app.add_track_queue.empty():
+            task = app.add_track_queue.get_nowait()
+            pc = task['pc']
+            peer = task['peer']
+            kind = task['kind']
+            if kind == 'video' and app.chats[peer]['video_track']:
+                pc.addTrack(app.chats[peer]['video_track'])
+                app.chats[peer]['video_track'] = None
+            elif kind == 'audio' and app.chats[peer]['audio_track']:
+                pc.addTrack(app.chats[peer]['audio_track'])
+                app.chats[peer]['audio_track'] = None
+            else:
+                app.add_track_queue.put(task)
+        await asyncio.sleep(3)
+
+
 
 @app.route("/")
 async def home(request):
@@ -32,7 +71,7 @@ async def home(request):
         result = []
         for record in records:
             result.append(dict(record))
-        return json({"result": result})
+        return json_response({"result": result})
 
 
 @app.route("/stream/<stream_id:int>")
@@ -93,11 +132,108 @@ async def add_show(request):
                 wall_id, stream_key, private
             )
         await inform_followers(user_id, stream)
-        return json({'stream_id': stream['id']})
+        return json_response({'stream_id': stream['id']})
     else:
         with open('templates/add.html') as f:
             content = f.read()
             return html(content)
+
+
+@app.route("/stream/<stream_id:int>/send", methods=["GET", "POST"])
+async def send_stream(request, stream_id):
+    async with app.config.pg_pool.acquire() as connection:
+        stream = await connection.fetchrow(
+            """
+            select * from stream
+            where id = $1
+            """,
+            stream_id
+        )
+        if not stream:
+            return html('Permission_denied')
+        stream_key = stream['stream_key']
+    with open('templates/send.html') as f:
+        content = f.read()
+        content = content.replace('{{stream_key}}', stream['stream_key'])
+        return html(content)
+
+
+@app.route("/offer/", methods=["GET", "POST"])
+async def offer(request):
+    params = request.json
+    offer = RTCSessionDescription(sdp=params["sdp"], type=params["type"])
+    stream_key = params["stream_key"]
+    peer = app.chats[stream_key]['peer']
+
+    pc = RTCPeerConnection()
+    pc_id = "PeerConnection(%s)" % uuid4()
+    pcs.add(pc)
+
+    def log_info(msg, *args):
+        logger.info(pc_id + " " + msg, *args)
+
+    recorder = MediaRecorder(f'rtmp://localhost:1935/show/{stream_key}', format='flv', options={
+        'framerate': '1',
+        'video_size': '720x404', 
+        'vcodec': 'libx264',
+        'maxrate': '768k',
+        'bufsize': '8080k',
+        'vf': '"format=yuv420p"',
+        'g': '60'
+    })
+
+    @pc.on("datachannel")
+    def on_datachannel(channel):
+        @channel.on("message")
+        def on_message(message):
+            if isinstance(message, str) and message.startswith("ping"):
+                channel.send("pong" + message[4:])
+
+    @pc.on("iceconnectionstatechange")
+    async def on_iceconnectionstatechange():
+        log_info("ICE connection state is %s", pc.iceConnectionState)
+        if pc.iceConnectionState == "failed":
+            await pc.close()
+            pcs.discard(pc)
+
+    @pc.on("track")
+    def on_track(track):
+        log_info("Track %s received", track.kind)
+
+        if track.kind == "audio":
+            app.chats[stream_key]['audio_track'] = track
+            app.add_track_queue.put({'pc': pc, 'peer': peer, 'kind': track.kind})
+            # pc.addTrack(track)
+        elif track.kind == "video":
+            # local_video = VideoTransformTrack(
+            #     track, transform=params["video_transform"]
+            # )
+            app.chats[stream_key]['video_track'] = track
+            app.add_track_queue.put({'pc': pc, 'peer': peer, 'kind': track.kind})
+            # pc.addTrack(track)
+
+            # recorder.addTrack(track)
+
+        @track.on("ended")
+        async def on_ended():
+            log_info("Track %s ended", track.kind)
+            await recorder.stop()
+
+    # handle offer
+    await pc.setRemoteDescription(offer)
+    # await recorder.start()
+
+    # send answer
+    answer = await pc.createAnswer()
+    await pc.setLocalDescription(answer)
+    print(pc.getTransceivers()[0].currentDirection)
+
+
+    return json_response({
+        "sdp": pc.localDescription.sdp,
+        "type": pc.localDescription.type
+    })
+
 
 
 async def inform_followers(user_id, stream):
@@ -111,4 +247,5 @@ async def inform_followers(user_id, stream):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+    app.add_task(notify_server_started_after_five_seconds)
+    app.run(host="0.0.0.0", port=8443, ssl=context, workers=1)
